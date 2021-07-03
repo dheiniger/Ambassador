510d38117f644c63cc4c4e4308e9ce8445679896
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2021-07-02 22:16:42 -0500

    Fixing tests

---
+++ test/ambassador/properties/live.clj
@@ -0,0 +1,9 @@
+(ns ambassador.properties.live
+  (:require [clojure.test :refer :all]))
+
+(def db
+  {:dbtype "mysql"
+   :host "*****"
+   :dbname "*****"
+   :user "*****"
+   :password "*****"})
